4302bc414e0cc9185f9c9ea3316f1747a03df5f4
XINCGer <1790617178@qq.com>
2017-12-14 22:52:42 +0800

    重建

--- Clipboard/proj.android/bin/classes/com/enhance/gameservice/IGameTuningService.java.d
+++
@@ -1,3 +0,0 @@
-F:\GitHub_Project\Cocos2d-X_Tools\Clipboard\proj.android\gen\com\enhance\gameservice\IGameTuningService.java: \
-  F:\GitHub_Project\Cocos2d-X_Tools\Clipboard\proj.android\src\com\enhance\gameservice\IGameTuningService.aidl 
-
--- Clipboard/proj.android/gen/org/cocos/Clipboard/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package org.cocos.Clipboard;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
